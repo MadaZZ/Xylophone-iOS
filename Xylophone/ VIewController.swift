c7872675d1b655b054c632fff3db022202bf52f3
MadaZZ <sparshranaazz@gmail.com>
2018-10-19 18:58:25 +0530

    Added sound playing functionality

--- Xylophone/ VIewController.swift
+++ Xylophone/ VIewController.swift
@@ -7,19 +7,35 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController{
     
-
+    var audioPlayer = AVAudioPlayer()
+    
+    let sounds = ["note1", "note2", "note3", "note4", "note5", "note6", "note7"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
 
-
     @IBAction func notePressed(_ sender: UIButton) {
+        playSound(soundFileName: sounds[sender.tag - 1])
+    }
+    
+    func playSound(soundFileName : String){
         
+        let soundURL = Bundle.main.url(forResource: soundFileName, withExtension: "wav")
         
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: soundURL!)
+        }
+        catch {
+            print(error)
+        }
+        
+        audioPlayer.play()
         
     }
     
